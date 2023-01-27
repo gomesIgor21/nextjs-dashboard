0236850a4b693594356a4a098274b01c4cde48c1
EvahIO <dev@evah.io>
2023-01-27 18:06:19 -0300

    Added Header and Sidebar

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between px-4 py-4'>
+      <h2>Dashboard</h2>
+      <h2>Welcome back, Igor</h2>
+    </div>
+  )
+}
+
+export default Header
